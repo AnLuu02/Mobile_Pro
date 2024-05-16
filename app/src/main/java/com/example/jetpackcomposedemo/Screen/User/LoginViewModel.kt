18679e6fd2f820bb62d12120f1fb0466a5c40fa0
AnLuu02 <anluu099@gmail.com>
2024-05-16 14:15:19 +0700

    Update LoginViewModel.kt

--- app/src/main/java/com/example/jetpackcomposedemo/Screen/User/LoginViewModel.kt
+++ app/src/main/java/com/example/jetpackcomposedemo/Screen/User/LoginViewModel.kt
@@ -6,9 +6,7 @@ import android.util.Log
 import android.widget.Toast
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
@@ -24,7 +22,7 @@ import com.google.firebase.auth.PhoneAuthCredential
 import com.google.firebase.auth.PhoneAuthOptions
 import com.google.firebase.auth.PhoneAuthProvider
 import com.google.firebase.auth.auth
-import com.google.firebase.database.database
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -181,6 +179,7 @@ class LoginViewModel(private val userRepository: UserRepository) : ViewModel() {
         viewModelScope.launch {
            try {
                val info = userRepository.getUserByPhone(phone)
+               delay(500)
                _uiState.update { currentState ->
                    currentState.copy(
                        id = info[0].ID!!,
@@ -196,7 +195,7 @@ class LoginViewModel(private val userRepository: UserRepository) : ViewModel() {
                initBirthday = formatBirthday()
                updateBirthday(initBirthday)
            } catch (e: Exception) {
-               logout()
+               Log.e("<Catch Login>",e.printStackTrace().toString())
            }
         }
     }
@@ -351,6 +350,7 @@ class LoginViewModel(private val userRepository: UserRepository) : ViewModel() {
                     if (phoneNumber != null) {
                         val validPhone = phoneNumber.replaceFirst("+84","0")
                         val myUser = MyUser(
+                            fullName = "User $validPhone",
                             sdt = validPhone
                         )
                         if (isLoginWithGoogle) {
